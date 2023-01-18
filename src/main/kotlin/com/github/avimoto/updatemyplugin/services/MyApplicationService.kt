2301cb8f13e80b1f594f6463d5c08ab5436eb89a
GitHub Action <action@github.com>
2023-01-18 17:58:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/avimoto/updatemyplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/avimoto/updatemyplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.avimoto.updatemyplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.avimoto.updatemyplugin.MyBundle
 
 class MyApplicationService {
 
